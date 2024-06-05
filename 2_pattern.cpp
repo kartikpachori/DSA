93663c2133524787ab9a58a96b033743e8235f52
kartikpachori <kartikeypachori1610@gmail.com>
2024-06-05 06:23:25 +0530

    commit in past

--- 2_pattern.cpp
+++ 2_pattern.cpp
@@ -14,7 +14,7 @@ int main()
         col = 1;
         while (col <= n)
         {
-            cout << "*&";
+            cout << "12";
             col = col + 1;
         }
         cout << endl;
